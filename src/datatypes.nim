ec1cc024656f86445457518914827ac584641187
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2022-04-03 14:53:12 -0400

    add fuzzy boolean type

---
+++ src/datatypes.nim
@@ -0,0 +1,15 @@
+import std/strutils
+
+
+type
+    FuzzyBool* = enum
+        fbTrue, fbFalse, fbUncertain
+
+proc parseFuzzyBool* (s: string): FuzzyBool =
+    let s2 = s.strip()
+    if s2.startsWith("t") or s2.startsWith("y"):
+        result = fbTrue
+    elif s2.startsWith("f") or s2.startsWith("n"):
+        result = fbFalse
+    else:
+        result = fbUncertain
