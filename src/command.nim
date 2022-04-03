d01a8b2db12a1b8c913f07e7bfe95de04b09a7da
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2022-04-03 14:54:27 -0400

    add functions to create a command or a subcommand

--- src/command.nim
+++ src/command.nim
@@ -22,3 +22,41 @@ type
         flagsLong*: Table[string, ref FlagVariant]
         sharedFlagsShort*: Table[char, ref FlagVariant]  # applies to all subcommands
         sharedFlagsLong*: Table[string, ref FlagVariant]  # applies to all subcommands
+
+
+proc process* (cv: CommandVariant): void =
+    echo(commandLineParams())
+
+proc new* (name: string,
+            info: string,
+            help: string,
+            callback: proc (input: varargs[string, `$`]): void
+            ): CommandVariant =
+    result = CommandVariant(name: name,
+                            info: info,
+                            help: help,
+                            subcommands: initTable[string, SubCommandVariant](),
+                            callback: callback,
+                            flagsShort: initTable[char, ref FlagVariant](),
+                            flagsLong: initTable[string, ref FlagVariant](),
+                            sharedFlagsShort: initTable[char, ref FlagVariant](),
+                            sharedFlagsLong: initTable[string, ref FlagVariant]()
+                            )
+
+proc addSubcommand* (cv: var CommandVariant,
+                        name: string,
+                        info: string,
+                        help: string,
+                        callback: proc (input: varargs[string, `$`]): void
+                        ): void =
+    cv.subcommands[name] = SubCommandVariant(kind: sckCommand, command:
+        CommandVariant(name: name,
+                        info: info,
+                        help: help,
+                        subcommands: initTable[string, SubCommandVariant](),
+                        callback: callback,
+                        flagsShort: initTable[char, ref FlagVariant](),
+                        flagsLong: initTable[string, ref FlagVariant](),
+                        sharedFlagsShort: initTable[char, ref FlagVariant](),
+                        sharedFlagsLong: initTable[string, ref FlagVariant]()
+                        ))
