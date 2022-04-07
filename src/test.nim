5637e10c87bdc3d1ada664e88858d07482713aa9
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2022-04-07 03:32:02 -0400

    update test

--- src/test.nim
+++ src/test.nim
@@ -13,13 +13,16 @@ proc bar(input: varargs[string, `$`]): void =
 proc bat(input: varargs[string, `$`]): void =
     echo("bat callback")
 
-proc foo_a(input: bool): void =
+proc foo_a(val: bool): void =
     echo("foo -a callback")
+
+proc foo_bbb(val: bool): void =
+    echo("foo --bbb callback")
 
 var cv1 = newCommandVariant("root", "test_short", "test_long", root)
 var cv_foo = cv1.addSubcommand("foo", "foo_short", "foo_long", foo)
 var cv_bar = cv1.addSubcommand("bar", "bar_short", "bar_long", bar)
 var cv_bat = cv1.addSubcommand("bat", "bat_short", "bat_long", bat)
 var cv_foo_alias = cv1.addAlias("foo_alias", "^,foo")
-cv_foo.addBoolFlag(shortName: 'a', callback: foo_a)
+discard cv_foo.addBoolFlag('a', foo_a, false).addBoolFlag("bbb", foo_bbb, false)
 cv1.process()
