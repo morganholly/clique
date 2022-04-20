c2c299b40ffb8fd7beb822e099fc302f6eb6a7ee
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2022-04-19 23:18:52 -0400

    test with generic addFlag

--- src/test.nim
+++ src/test.nim
@@ -30,27 +30,27 @@ var dref = new int64
 dref[] = 5
 
 
-dumptree:
-    flag = FlagVariantRef(kind: fkShortAndLong,
-                            shortName: 'c',
-                            longName: "cc",
-                            datatype: itInt,
-                            valInt: 0,
-                            actionInt: faCallback,
-                            callbackInt: bar_cc,
-                            hasNoInputAction: nikHasNoInputAction,
-                            noInputType: itInt,
-                            noInputBool: false,
-                            noInputAction: faCallback,
-                            callbackNoInput: bar_cc_ni)
-    com.flagsLong[longName] = flag
+# dumptree:
+#     flag = FlagVariantRef(kind: fkShortAndLong,
+#                             shortName: 'c',
+#                             longName: "cc",
+#                             datatype: itInt,
+#                             valInt: 0,
+#                             actionInt: faCallback,
+#                             callbackInt: bar_cc,
+#                             hasNoInputAction: nikHasNoInputAction,
+#                             noInputType: itInt,
+#                             noInputBool: false,
+#                             noInputAction: faCallback,
+#                             callbackNoInput: bar_cc_ni)
+#     com.flagsLong[longName] = flag
 
 var cv1 = newCommandVariant("root", "test_short", "test_long", root)
 var cv_foo = cv1.addSubcommand("foo", "foo_short", "foo_long", foo)
 var cv_bar = cv1.addSubcommand("bar", "bar_short", "bar_long", bar)
 var cv_bat = cv1.addSubcommand("bat", "bat_short", "bat_long", bat)
 var cv_foo_alias = cv1.addAlias("foo_alias", "^,foo")
-var cv_foo_flags = cv_foo.addBoolFlag('a', foo_a, false).addBoolFlag("bbb", foo_bbb, false)
-var cv_bar_flags = cv_bar.addFlag('d', "", dref, nil, false)
+var cv_foo_flags = cv_foo.addFlag('a', foo_a).addFlag("bbb", foo_bbb)
+var cv_bar_flags = cv_bar.addFlag('d', dref)
 # addFlags(cv_bar, ('c', "cc", true, bar_cc), ('d', true, dref))
 cv1.process()
